0bfdcdbc0fac948eb4a8156a83ed221f81354ccb
Hélvio Filho <helviosvf@gmail.com>
2021-06-25 02:06:41 -0300

    SmallInput Component

---
+++ src/components/SmallInput/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    fontFamily: theme.fonts.text400,
+    fontSize: 13,
+    width: 48,
+    height: 48,
+    backgroundColor: theme.colors.secondary40,
+    color: theme.colors.heading,
+    borderRadius: 8,
+    marginRight: 4,
+    textAlign: 'center',
+  }
+});
