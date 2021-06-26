127aa5b27881658c1967e02c87993a09509dc623
Hélvio Filho <helviosvf@gmail.com>
2021-06-26 03:48:18 -0300

    configs created

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,13 @@
+const REDIRECT_URI = 'https%3A%2F%2Fauth.expo.io%2F%40anonymous%2Fgameplay-399fb964-4648-48ec-9d6b-ae96deecf59f';
+const SCOPE = 'identify%20email%20connections%20guilds';
+const RESPONSE_TYPE = 'token';
+const CLIENT_ID = '858163083997872179';
+const CDN_IMAGE = 'https://cdn.discordapp.com';
+
+export {
+  REDIRECT_URI,
+  SCOPE,
+  RESPONSE_TYPE,
+  CLIENT_ID,
+  CDN_IMAGE
+}
